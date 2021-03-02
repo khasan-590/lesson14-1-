14855c3d4093c3673156b2886d3b77e372f6fdce
khasan-590 <khasan4ik590@gmail.com>
2021-03-02 20:28:34 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const asking = prompt('Пожалйста введите . или #');
+
 function DomElement   (selector, height,  width, bg, fontSize ) {
   this.selector = selector;
   this.height = height;
@@ -8,22 +10,34 @@ function DomElement   (selector, height,  width, bg, fontSize ) {
   this.fontSize = fontSize;
 }
 
-
 DomElement.prototype.strElements = function (){
-  if (this.selector[0] === '.') {
+  
+  if (asking.this.selector[0] === '.') {
   let divItem =  document.createElement('div');
   document.body.append(divItem);
+  // divItem.classList.add(selector.slice(1));
+  divItem.style.cssText `selector: ${this.selector};
+  height: ${this.height};
+  width: ${this.width};
+  background: ${this.bg};
+  font-size: ${this.fontSize}; `;
+
   console.log('blocks');
-} else if (this.selector[0] === '#') {
+} else if (asking.this.selector[0] === '#') {
   let pItem = document.createElement('p');
-  document.body.append(pItem);
   
+  document.body.append(pItem);
+  pItem.style.cssText `selector: ${this.selector};
+  height: ${this.height};
+  width: ${this.width};  
+  background: ${this.bg};
+  font-size: ${this.fontSize}; `;
 }
 };
 
-
 let str1 = new DomElement('.simple' , '50px' , '60px', 'black', 'Sans-serif');
-
+let str2 = new DomElement('#simple' , '50px' , '60px', 'black', 'Sans-serif');
 
 
 console.log(str1);
+console.log(str2);

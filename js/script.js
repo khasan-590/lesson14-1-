90d0f281e7353a7eb9ae90fccfc286fcc47a72c0
khasan-590 <khasan4ik590@gmail.com>
2021-03-02 22:50:33 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -14,8 +14,8 @@ DomElement.prototype.strElements = function (){
   if (this.selector[0] === '.') {
   let divItem =  document.createElement('div');
   document.body.append(divItem);
-  // divItem.classList.add(selector.slice(1));
-  divItem.style.cssText ` height: ${this.height};
+  
+  divItem.style.cssText = ` height: ${this.height};
   width: ${this.width};
   background: ${this.bg};
   font-size: ${this.fontSize}; `;
@@ -23,7 +23,7 @@ DomElement.prototype.strElements = function (){
 } else if (this.selector[0] === '#') {
   let pItem = document.createElement('p');
   document.body.append(pItem);
-  pItem.style.cssText ` height: ${this.height};
+  pItem.style.cssText = ` height: ${this.height};
   width: ${this.width};  
   background: ${this.bg};
   font-size: ${this.fontSize}; `;
@@ -33,5 +33,7 @@ DomElement.prototype.strElements = function (){
 let str1 = new DomElement(asking , '50px' , '60px', 'black', '40px');
 let str2 = new DomElement(asking , '50px' , '60px', 'black', '40px');
 
+str1.strElements();
+str2.strElements();
 console.log(str1);
 console.log(str2);
